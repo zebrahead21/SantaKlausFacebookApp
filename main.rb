ca8ced325dd8f60b15be6ea016e97c8fce93d30c
catalinmititiuc <mititiuc.cata@gmail.com>
2016-10-23 05:48:23 -0700

    final markov

--- main.rb
+++ main.rb
@@ -4,14 +4,27 @@ require_relative './lib/markov.rb'
 
 markov = Markov.new
 
-texts = File.read('./samples/sample_1').force_encoding("utf-8")
+if File.exists?('./tmp/state')
+	puts "Exista"
+	dump = File.read('./tmp/state')
+	markov = Marshal.load(dump)
+else
+	texts = File.read('./samples/sample_1').force_encoding("utf-8")
 
-markov.feed(texts)
-markov.normalize!
+	markov.feed(texts)
+	markov.normalize!
+end
+
 post = markov.generate_phrase()
 
 puts post
 
 api = Koala::Facebook::API.new(FB_APP_TOKEN)
-# api.put_wall_post(post)
+api.put_wall_post(post)
 
+unless File.exists?('./tmp/state')
+	puts "Does not exist"
+	File.open('./tmp/state', 'w') do |file|
+		Marshal.dump(markov, file)
+	end
+end
